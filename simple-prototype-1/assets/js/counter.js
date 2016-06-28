3f73cc7c207dd462d7077786ed42be3eb6de103c
JJOL <jjoulk@gmail.com>
2016-06-28 13:59:26 -0500

    Footer Policy Message - index.html  && Time Calculation Added to counter.js

--- simple-prototype-1/assets/js/counter.js
+++ simple-prototype-1/assets/js/counter.js
@@ -1,5 +1,23 @@
 (function(){
 
+  var now, munDate, timeDiff, diffDate, diffH, diffHours, diffM, diffMinutes, dayConv, hrsConv;
+  now = new Date();
+  tjmunDate = new Date(2016, 11, 8);
+
+  timeDiff = Math.abs(tjmunDate.getTime() - now.getTime());
+  diffDate = new Date(timeDiff);
+
+  hrsConv = 1000*3600;
+  dayConv = hrsConv*24;
+
+  diffDays    = Math.floor(timeDiff / (dayConv) );
+  diffH       = diffDate - (diffDays * (dayConv) )
+  diffHours   = Math.floor(diffH / (hrsConv) );
+
+
+  diffM       = diffH - (diffHours * (hrsConv) );
+  diffMinutes = Math.floor(diffM / (1000 * 60) );
+
 
   var imgEl = document.getElementById('counter-img'),
       secEl = document.getElementById('label-sec'),
@@ -9,9 +27,9 @@
       degs = 0,
       incr = 360/60,
       secs = 59,
-      mins = 10,
-      hrs  = 10,
-      days = 10;
+      mins = diffMinutes,
+      hrs  = diffHours,
+      days = diffDays;
   // imgEl.style.transform = "rotateZ("+ degs +"deg)";
   setInterval(function() {
     secEl.innerHTML = secs;
