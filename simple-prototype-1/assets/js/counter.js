3ac369aa867bd95cb62002cbcb86a5ec29e8ce0b
JJOL <jjoulk@gmail.com>
2016-07-06 21:11:39 -0500

    Page Lookout

--- simple-prototype-1/assets/js/counter.js
+++ simple-prototype-1/assets/js/counter.js
@@ -8,8 +8,9 @@
   //TODO Problem Solved By switching off degrees/transform incr when page is not visible, however it can cause some
   // unsync time errors
 
+  var hidden, visibilityChange,
+  isPageVisible = true;
 
-  var hidden, visibilityChange;
   if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
     hidden = "hidden";
     visibilityChange = "visibilitychange";
@@ -24,22 +25,39 @@
     visibilityChange = "webkitvisibilitychange";
   }
 
-  var pageIsVisible = true;
+  if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
+    // Browser Doesn't Support Visibility API so we have to know how many people don't support it
+    console.log('Browser Doesn\'t Support Visibility API!');
+  } else {
+    console.log('Page Visibility Listener Registered');
+    document.addEventListener(visibilityChange, handleVisibility, false);
+  }
 
+  function handleVisibility() {
+    if (document[hidden]) {
+      console.log('Document was hidden');
+      isPageVisible = false;
+    } else {
+      console.log('Document was shown');
+      isPageVisible = true;
+    }
+  }
+
+  // Clock
   var now, munDate, timeDiff, diffDate, diffH, diffHours, diffM, diffMinutes, dayConv, hrsConv;
+
   now = new Date();
   tjmunDate = new Date(2016, 11, 8);
 
   timeDiff = Math.abs(tjmunDate.getTime() - now.getTime());
   diffDate = new Date(timeDiff);
 
-  hrsConv = 1000*3600;
-  dayConv = hrsConv*24;
+  hrsConv = 1000 * 3600;
+  dayConv = hrsConv * 24;
 
   diffDays    = Math.floor(timeDiff / (dayConv) );
   diffH       = diffDate - (diffDays * (dayConv) )
   diffHours   = Math.floor(diffH / (hrsConv) );
-
 
   diffM       = diffH - (diffHours * (hrsConv) );
   diffMinutes = Math.floor(diffM / (1000 * 60) );
@@ -56,26 +74,27 @@
       mins = diffMinutes,
       hrs  = diffHours,
       days = diffDays;
-  // imgEl.style.transform = "rotateZ("+ degs +"deg)";
 
 
   setInterval(function() {
+
+    // Update Label Values
     secEl.innerHTML = secs;
     minEl.innerHTML = mins;
     hrsEl.innerHTML = hrs;
     dayEl.innerHTML = days;
 
-    imgEl.style.transform = "rotateZ("+ degs +"deg)";
-    // if(degs == 360) degs = 0;
-    if(pageIsVisible)
+    // If Page Is Not Visible Don't Rotate The Image so it doesn't get buggy after page becomes visible again
+    if(isPageVisible)
       degs += incr;
+
+    // Rotate Image
+    imgEl.style.transform = "rotateZ("+ degs +"deg)";
+
     secs--;
     if(secs <= -1) {
       secs = 59;
       mins--;
-      // degs+=360;
-      // imgEl.style.transform = "rotateZ("+ degs +"deg)";
-
     }
     if(mins <= -1) {
       mins = 59;
@@ -85,27 +104,12 @@
       hrs = 23;
       days--;
     }
-  }, 1000);
-
-  if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
-    console.log('Browser Doesn\'t Support Visibility API!');
-  } else {
-    console.log('Adding Visibility Listener');
-    document.addEventListener(visibilityChange, handleVisibility, false);
-  }
-  //imgEl.classList.remove('animatable');
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-  function handleVisibility() {
-    console.log('Visibility Has Changed!');
-    if (document[hidden]) {
-      console.log('Document was hidden');
-      pageIsVisible = false;
-    } else {
-      console.log('Document was shown');
-      pageIsVisible = true;
+    if(days <= -1) {
+        //TODO MUN Starts HERE!!!
     }
 
-  }
+  }, 1000);
+
+
 
 })();
