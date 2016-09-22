6f9161985a3d4e1f99521f7ca704444905de3c0a
JJOL <jjsystems.jjol@gmail.com>
2016-09-22 00:11:10 -0500

    Registration

---
+++ simple-prototype-1/templates/registration/form-init.js
@@ -0,0 +1,73 @@
+var parent = document.getElementById('google-form-container');
+var gform = document.getElementById('gform');
+function calculateFormSize() {
+  var window_width = window.innerWidth;
+  var window_height = window.innerHeight;
+
+
+  var form_width = (window_width * 0.8) + 'px';  // Get the 80% of the window width
+  var form_height = '400px';
+
+  parent.style.width = form_width;
+  parent.style.height = form_height;
+
+  gform.style.width = form_width;
+  gform.style.height = form_height;
+}
+setTimeout(function() {
+  calculateFormSize();
+}, 100);
+
+window.onresize = function(e) {
+  calculateFormSize();
+}
+
+
+var schoolUserName = 'school',
+    independentUserName = 'independent',
+    nullUserName = 'unknown';
+
+var vmApp = new Vue({
+  el: '#register-form',
+  data: {
+    typeOfUser: nullUserName,
+    independentHead: false,
+    schoolHead: false
+  },
+  computed: {
+    isSchoolForm: function() {
+      return this.typeOfUser == schoolUserName;
+    },
+    isIndependentForm: function() {
+      return this.typeOfUser == independentUserName;
+    }
+  },
+  methods: {
+    openSchoolForm: function() {
+      var prevType = this.typeOfUser,
+          newType = schoolUserName;
+      this.typeOfUser = schoolUserName;
+      this.updateView(prevType, newType);
+    },
+    openIndepententForm: function() {
+      var prevType = this.typeOfUser,
+          newType = independentUserName;
+      this.typeOfUser = schoolUserName;
+      this.updateView(prevType, newType);
+    },
+    updateView: function(prevType, newType) {
+      if (prevType == newType)
+        return;
+
+        this.schoolHead = this.independentHead = false;
+
+      if(newType == schoolUserName) {
+        this.schoolHead = true;
+      }
+      else if(newType == independentUserName) {
+        this.independentHead = true;
+      }
+
+    }
+  }
+});
