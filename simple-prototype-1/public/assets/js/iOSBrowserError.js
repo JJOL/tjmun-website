a7df03d0ca3bf313dc147dc860f71e8d1dd1bbfc
JJOL <jjoulk@gmail.com>
2016-10-14 08:39:21 -0500

    Fixed Issue by modifying libs/bower/webcomponents/webcomponents.js  > getDescriptor()-dummyDescriptor  -- Found issue on github

--- simple-prototype-1/public/assets/js/iOSBrowserError.js
+++ simple-prototype-1/public/assets/js/iOSBrowserError.js
@@ -1,17 +1,20 @@
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+var supportSafariIOS = true;
+if (!supportSafariIOS) {
+  var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  isIOS = true;
+  if (isSafari && isIOS) {
+    var errDis = document.getElementById('full-error-view');
+    if(errDis) {
+      errDis.style.display = 'block';
 
-if (isSafari && isIOS) {
-  var errDis = document.getElementById('full-error-view');
-  if(errDis) {
-    errDis.style.display = 'block';
+      var btn = document.getElementById('full-error-view-button');
+      btn.onclick = function() {
+        errDis.style.display = 'none';
+      }
 
-    var btn =document.getElementById('full-error-view-button');
-    btn.onclick = function() {
-      errDis.style.display = 'none';
+    } else {
+      console.log('Err display not found!')
     }
-
-  } else {
-    console.log('Err display not found!')
   }
 }
